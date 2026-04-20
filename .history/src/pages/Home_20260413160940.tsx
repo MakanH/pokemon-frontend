1825cd219feecac0fe44464426bcc1e7e8f6dc70
Makan Homayoun <makan.h07@gmail.com>
2026-04-20 14:59:50 -0400

    3.6-UI-design-start

---
+++ .history/src/pages/Home_20260413160940.tsx
@@ -0,0 +1,31 @@
+import type { JSX } from "react";
+import { SinglePokemon } from "../components/SinglePokemon.tsx";
+import { AllPokemons } from "../components/AllPokemons.tsx";
+import { AddPokemon } from "../components/AddPokemon.tsx";
+import { UpdatePokemon } from "../components/UpdatePokemon.tsx";
+
+/** Main component for our home page */
+export function Home(): JSX.Element {
+  return (
+    <>
+      <h1>Welcome to our Pokemon App</h1>
+      {/* Step 3.4a link test */}
+      <a href="http://localhost:5174/pokemons/charizard">
+        Find Eevee (Raw JSON)
+      </a>
+      <br />
+      <br />
+
+      <SinglePokemon />
+      <br />
+      <hr />
+      <AllPokemons />
+      <br />
+      <hr />
+      <AddPokemon />
+      <br />
+      <hr />
+      <UpdatePokemon />
+    </>
+  );
+}
